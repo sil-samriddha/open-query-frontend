142fbfff90e19ba90bdcb273e2bde6354deaf126
Samriddha Sil <samriddha.sil297@gmail.com>
2023-04-05 21:49:56 +0530

    mobile viewport updated

--- src/pages/Chatarea.jsx
+++ src/pages/Chatarea.jsx
@@ -35,16 +35,15 @@ function Chatarea(props) {
   const sendMessage = async(message)=>{
     await Promise.resolve(setChat((prev)=>[...prev, message]))
     mutation.mutate();
-
   }
   return (
     <div
-      className={`h-screen w-screen py-6 relative sm:px-16 px-12 text-${
+      className={`h-[90vh] sm:h-screen py-6 relative sm:px-16 px-10 text-${
         props.mode === "black" ? "white" : "[#1A232E]"
-      } overflow-hidden flex flex-col justify-between  align-middle`}
+      } overflow-hidden flex flex-col justify-between align-middle`}
       style={
         props.mode === "black"
-          ? { background: "#1A232E" }
+          ? { background: "#1A232E"}
           : { background: "white" }
       }
     >
